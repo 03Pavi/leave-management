2f72cb740b9f0cbdd8b98b93bb3ca09cac10926e
Pavitar Singh <71433170+Pavitar01@users.noreply.github.com>
2024-06-06 18:48:07 +0530

    feature: Add Login screen ui

--- src/screens/login-page/index.tsx
+++ src/screens/login-page/index.tsx
@@ -1,17 +1,33 @@
+"use client"
 import React from 'react'
 import { Authenticate } from '@/assets/images'
-import { Box, Typography } from '@mui/material'
+import { Box, Button, Divider, TextField, Typography } from '@mui/material'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { styled } from '@mui/material/styles';
 import "./styles.css"
+const Root = styled('div')(({ theme }) => ({
+    marginTop: "20px",
+    width: '100%',
+    ...theme.typography.body2,
+    color: theme.palette.text.secondary,
+    '& > :not(style) ~ :not(style)': {
+        marginTop: theme.spacing(2),
+    },
+}));
 const LoginPage = () => {
+    const router = useRouter()
+    const handleLogin = () => {
+        router.push("/welcome")
+    }
     return (
         <Box className="login-page-wrapper">
             <Box className="right">
-                <Image src={Authenticate} alt='' />
+                <Image src={Authenticate} alt='' width={500} className='login-image' />
             </Box>
             <Box className="left">
                 <Box className="navigate-link">
-                    <Typography component="p">Not Register?<Typography component="span">Create an account</Typography></Typography>
+                    <Typography component="p">Welcome! To<Typography component="span">&nbsp;Leave Management System</Typography></Typography>
                 </Box>
                 <Box className="login-page-container">
                     <Box className="login-form">
@@ -19,12 +35,36 @@ const LoginPage = () => {
                             <Typography className='title' component="span">Welcome back! 👋</Typography>
                             <Typography className='description' component="p">Login to your account</Typography>
                         </>
-                       <Box className="login-input">
-                       </Box>
+                        <Box className="login-input">
+                            <Box mb={2} className="login-input-field">
+                                <Typography component="p">UserName / Email</Typography>
+                                <TextField
+                                    className='text-field'
+                                    variant="outlined"
+                                    fullWidth
+                                />
+                            </Box>
+                            <Box mb={2} className="login-input-field">
+                                <Typography component="p">Password</Typography>
+                                <TextField
+                                    className='text-field'
+                                    variant="outlined"
+                                    fullWidth
+                                />
+                            </Box>
+                            <Button onClick={handleLogin} sx={{ bgcolor: "#0067FF", marginBottom: "10px", height: "40px" }} variant='contained' fullWidth>
+                                Login
+                            </Button>
+                        </Box>
+                        <Typography component="span" className='forget-password-link'>Forget Password?</Typography>
+                        <Root>
+                            <Divider sx={{ color: "var(--black)", }}>
+                                or
+                            </Divider>
+                        </Root>
                     </Box>
 
                 </Box>
-
             </Box>
         </Box>
     )
